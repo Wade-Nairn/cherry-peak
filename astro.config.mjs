689fe511aa709235051875beb9d9709b3f7b8ba1
Wade Nairn <wadenairn@gmail.com>
2024-06-18 11:06:10 +1000

    Added GA4

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,19 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
-
+import partytown from '@astrojs/partytown'
 // https://astro.build/config
 export default defineConfig({
   site: "https://cherrypeak.digital",
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [
+    tailwind(), 
+    mdx(), 
+    sitemap(), 
+    icon(),		
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+],
 });
